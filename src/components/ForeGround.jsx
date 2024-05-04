75f7ae48ef0a5ad5e1bcd92dfd4e2fa5b9f67121
Saketh-Reddy-Bejadi <bejadisakethreddy168@gmail.com>
2024-05-04 23:39:06 +0530

    modified

--- src/components/ForeGround.jsx
+++ src/components/ForeGround.jsx
@@ -1,24 +1,40 @@
 import { useState } from "react";
 
 const ForeGround = () => {
-  const [task, setTask] = useState("");
+  // const [task, setTask] = useState("");
   const [taskName, setTaskName] = useState("");
+  const [desc, setDesc] = useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const task = {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const t = {
       id: Math.floor(Math.random() * 10),
       name: taskName,
+      des: desc,
     };
+    // if (t.name === "" || t.des === "") {
+    //   alert("Task Name or Description cannot be empty...");
+    // } else {
+    //   setTask([...t, task]);
+    // }
+    console.log(t.name);
   };
 
   return (
-    <div className="bg-black w-dvw h-dvh flex pt-20 justify-center">
-      <form action="" onSubmit={handleSubmit}>
+    <div className="bg-black w-dvw h-dvh pt-20 justify-center">
+      <form onSubmit={handleSubmit}>
         <input
           onChange={(e) => setTaskName(e.target.value)}
           type="text"
           value={taskName}
+          autoCapitalize="sentences"
+        />
+        <br />
+        <input
+          className="mt-10"
+          onChange={(e) => setDesc(e.target.value)}
+          type="text"
+          value={desc}
         />
       </form>
     </div>
